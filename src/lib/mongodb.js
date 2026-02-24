00aa25be09b53184dc543e94919b7e65623f4718
Jakawat <U6710833@au.edu>
2026-02-24 10:48:23 +0700

    fix mongodb connection pooling

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -6,14 +6,9 @@ export function getClientPromise() {
   const uri = process.env.MONGODB_URI;
   if (!uri) throw new Error("Please add your Mongo URI to .env.local");
 
-  if (process.env.NODE_ENV === "development") {
-    if (!globalClientPromise) {
-      const client = new MongoClient(uri, options);
-      globalClientPromise = client.connect();
-    }
-    return globalClientPromise;
-  } else {
+  if (!globalClientPromise) {
     const client = new MongoClient(uri, options);
-    return client.connect();
+    globalClientPromise = client.connect();
   }
+  return globalClientPromise;
 }
